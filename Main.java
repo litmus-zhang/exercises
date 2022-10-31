aac6610a94b4fec5b6b3493c70de074d6b54faa2
litmus-zhang <abdulsalamlukmon9@gmail.com>
2022-10-31 08:09:40 -0700

    exercise 1 done

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.print("Hello World");
+        System.out.print("Hello World 2");
+    System.out.println(3);
+    // Variables
+String name = "John";
+System.out.println(name);
+int myNum = 15;
+System.out.println(myNum + name);
+int NewNum;
+NewNum = myNum;
+System.out.println(NewNum);
+final int newMyNum = 15;
+
+System.out.println(newMyNum);
+float myFloatNum = 5.88f;
+System.out.println(myFloatNum);
+    }
+}
+
+
